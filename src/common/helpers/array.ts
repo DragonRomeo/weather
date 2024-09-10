03cbbe564d4b34adb147ae1e07bdb111028366d7
Ilya Dragonromeo <you@example.com>
2024-09-10 14:58:07 +0300

    [6][Feature] Add Helpers

---
+++ src/common/helpers/array.ts
@@ -0,0 +1,11 @@
+import { KeyOfValue } from '../types/common';
+
+export function arrayToObject<T = Record<string, unknown>>(
+  arr: T[],
+  keyField: KeyOfValue<T, string>
+): Record<string, T> {
+  return Object.assign(
+    {},
+    ...arr.map((item) => ({ [item[keyField] as string]: item }))
+  );
+}
