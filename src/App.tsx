e785cd2875080ea2867af2890aea0d158c013c12
Ilya Dragonromeo <you@example.com>
2024-09-10 17:52:42 +0300

    [11][Feature] Implement App Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import AppWrapper from './ui/app-wrapper/app-wrapper';
+import AppContainer from './ui/app-container/app-container';
+import AppContent from './ui/app-content/app-content';
 
 function App() {
   // @ts-ignore: Store is empty.
-  return <Provider store={store}></Provider>;
+  return (
+    <Provider store={store}>
+      <AppWrapper>
+        <AppContainer>
+          <AppContent />
+        </AppContainer>
+      </AppWrapper>
+    </Provider>
+  );
 }
 
 export default App;
