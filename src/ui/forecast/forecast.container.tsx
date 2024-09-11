9b0f39e6a838c6eec8155009f092290a131501ce
Ilya Dragonromeo <you@example.com>
2024-09-11 14:45:55 +0300

    [8][Feature] Add component Forecast.container & Forecast

---
+++ src/ui/forecast/forecast.container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Forecast from './forecast';
+
+const ForecastContainer = () => {
+  // get Redux data and transfer it
+  return <Forecast />;
+};
+
+export default ForecastContainer;
---
+++ src/ui/forecast/forecast.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Forecast = () => {
+  // Takes data only from 'father'
+  return <div>Forecast</div>;
+};
+
+export default Forecast;
