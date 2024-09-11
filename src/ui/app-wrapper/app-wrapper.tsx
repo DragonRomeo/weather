3f16a4a43cd20ead45d1c2f3dd2c98ed31ae0783
Ilya Dragonromeo <you@example.com>
2024-09-11 15:08:53 +0300

    [13][Feature] Find BG image & implement this (templorary)

--- src/ui/app-wrapper/app-wrapper.tsx
+++ src/ui/app-wrapper/app-wrapper.tsx
@@ -1,13 +1,21 @@
 import React, { ReactNode } from 'react';
 import { mainBgColor } from '../common/styles/consts';
+import bgImage from '../../assets/background_image/snow_bg_image.jpg';
 
 type Props = {
   children: ReactNode;
 };
 
 const AppWrapper: React.FC<Props> = ({ children }) => {
+  console.log(bgImage);
   return (
-    <div style={{ width: '100%', backgroundColor: mainBgColor }}>
+    <div
+      style={{
+        width: '100%',
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+      }}
+    >
       {children}
     </div>
   );
