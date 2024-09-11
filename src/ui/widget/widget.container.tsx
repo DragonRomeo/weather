c2fff71a0dc553f908c0016c1246f41f03bc108d
Ilya Dragonromeo <you@example.com>
2024-09-11 14:43:25 +0300

    [9][Feature] Add component Widget & Widget.container

---
+++ src/ui/widget/widget.container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Widget from './widget';
+
+const WidgetContainer = () => {
+  // get Redux data and transfer it
+  return <Widget />;
+};
+
+export default WidgetContainer;
---
+++ src/ui/widget/widget.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Widget = () => {
+    // Takes data only from 'father'
+  return (
+    <div>Widget</div>
+  )
+}
+
+export default Widget
