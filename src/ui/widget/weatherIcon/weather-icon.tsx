67cdd774ee130b52a85988d97e7397375a3f849e
Ilya Dragonromeo <you@example.com>
2024-09-13 13:26:21 +0300

    [29][Refactoring] Adjust icon component

--- src/ui/widget/weatherIcon/weather-icon.tsx
+++ src/ui/widget/weatherIcon/weather-icon.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 type Props = {
   icon: string;
+  width?: string;
+  height?: string;
 };
 
-const WeatherIcon: React.FC<Props> = ({ icon }) => {
-  return <img src={icon} width='64px' height='64px' alt='weather' />;
+const WeatherIcon: React.FC<Props> = ({
+  icon,
+  width = '64px',
+  height = '64px',
+}) => {
+  return <img src={icon} width={width} height={height} alt='weather_icon' />;
 };
 
 export default WeatherIcon;
