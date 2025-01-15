321f79b260d238cccecf211cffc0cf113b39db30
Ilya Dragonromeo <you@example.com>
2025-01-15 18:44:01 +0300

    [72][Feature] Add Loader

--- src/ui/forecast/weather-details/weather-details.wrapper.tsx
+++ src/ui/forecast/weather-details/weather-details.wrapper.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
 import WeatherDetails from './weather-details';
 import { useSelector } from 'react-redux';
 import { useGetWeatherQuery } from '../../../services/weather-service';
 import { IRootState } from '../../../common/types/store';
+import CircularLoader from '../../circular-loader/circural-loader';
 
 const WeatherDetailWrapper = () => {
-  //This is copypaste from widget.container. Its' should be only in one file.
-  //TODO: put this copypaste in a separate code
   const city = useSelector((state: IRootState) => state.city.name);
   const { data } = useGetWeatherQuery(city);
-  return (
-    <>
-      <WeatherDetails weather={data} />
-    </>
-  );
+  return <>{data ? <WeatherDetails weather={data} /> : <CircularLoader />}</>;
 };
 
 export default WeatherDetailWrapper;
