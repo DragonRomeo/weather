f3408afb017869bd66704d82c8a6532f004a0cae
Ilya Dragonromeo <you@example.com>
2025-01-21 14:56:11 +0300

    [38][Refactor] Change name of Folder weatherIcon => weather-icon

--- src/ui/widget/widget.tsx
+++ src/ui/widget/widget.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IWeather } from '../../common/types/IWeather';
 import Temperature from './temperature/temperature';
 import City from './city/city';
-import WeatherIcon from './weatherIcon/weather-icon';
+import WeatherIcon from './weather-icon/weather-icon';
 import { styled, Typography } from '@mui/material';
 import { temperatureFontSize } from '../../common/styles/consts';
 import DateContainer from './date-container/date-container';
