9dc62ccc5281c782f7dfc2dcc664876d187e9199
Ilya Dragonromeo <you@example.com>
2025-01-27 17:40:53 +0300

    [82][Feature] Replace details weather button with icon

--- src/ui/weather-box/weather-box.styles.ts
+++ src/ui/weather-box/weather-box.styles.ts
@@ -10,6 +10,5 @@ export const styles: SxStyles = {
     borderRadius: '5px',
     boxShadow:
       'box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-    padding: '10px 20px',
   },
 };
