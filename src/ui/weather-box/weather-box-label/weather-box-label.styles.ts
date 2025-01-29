623be066e34611d0af97832892946ea8ff97dd71
Ilya Dragonromeo <you@example.com>
2025-01-29 16:51:17 +0300

    [91][Feature] Add WeatherBoxIndicator component

---
+++ src/ui/weather-box/weather-box-label/weather-box-label.styles.ts
@@ -0,0 +1,13 @@
+import { muiBorderColor } from '../../../common/styles/consts';
+import { SxStyles } from '../../../common/types/common';
+
+export const styles: SxStyles = {
+  weather_label_container: { margin: '10px' },
+  weather_label_title_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '7px',
+    alignItems: 'center',
+    color: muiBorderColor,
+  },
+};
