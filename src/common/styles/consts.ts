7a354eac784331498b044158db62db3a2e5d61a3
Ilya Dragonromeo <you@example.com>
2025-02-05 17:11:18 +0300

    [105][Feature] Change background for drawer (black)

--- src/common/styles/consts.ts
+++ src/common/styles/consts.ts
@@ -13,3 +13,7 @@ export const temperatureFontSize = '8rem';
 export const muiBorderColor = 'rgba(255, 255, 255, 0.6)';
 
 export const blurBackground = 'blur(2px)';
+
+export const blurBackgroundDrawer = 'blur(30px)';
+
+export const darkBackgroundColor = 'rgba(0,0,0,0.6)';
