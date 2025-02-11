c62539ca846a699c78e2fa85e6fe4773cee525ff
Ilya Dragonromeo <you@example.com>
2025-02-11 14:46:19 +0300

    [103][Refactor] Remove extra helper

--- src/common/helpers/string.ts
+++ src/common/helpers/string.ts
@@ -1,21 +1,3 @@
-import { Nullable } from '../types/common';
-
-export function getFileExtensionByPath(path: string): Nullable<string> {
-  // Check if there is a period in the path
-  const periodIndex = path.lastIndexOf('.');
-  const lastSlashIndex = path.lastIndexOf('/');
-
-  // If no period is found, or the period is part of a directory (e.g., "/home/user/file.")
-  // Return null
-  if (periodIndex === -1 || periodIndex < lastSlashIndex) {
-    return null;
-  }
-
-  // Extract and return the file extension
-  const extension = path.substring(periodIndex + 1);
-  return extension.length > 0 ? extension : null;
-}
-
 export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
